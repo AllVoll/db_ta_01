46fe6b81019574fb12aab932cd9dcdb5452b046b
AllVoll <128129005+AllVoll@users.noreply.github.com>
2023-04-12 11:59:22 +0300

    Add files via upload

---
+++ api-db-ta/utils/logger.py
@@ -0,0 +1,27 @@
+#logger.py
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+logger.debug('Debug message')
+logger.info('Info message')
+logger.warning('Warning message')
+logger.error('Error message')
+logger.critical('Critical message')
+
---
+++ api-db-ta/web/database.py
@@ -0,0 +1,25 @@
+#database.py
+
+import asyncpg
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .base import Base
+from . import models
+
+
+DATABASE_URL = "postgresql://av:av@timescale/av"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+async def get_db():
+    async with asyncpg.create_pool(DATABASE_URL) as pool:
+        async with pool.acquire() as conn:
+            async with conn.transaction():
+                db = Session(engine)
+                try:
+                    yield db
+                finally:
+                    db.close()
